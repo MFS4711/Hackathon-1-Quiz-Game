48301b35cb7e8e363ec122c06aa0d96e214fcf36
Muhammad Shabir <n48jomzr@students.codeinstitute.net>
2024-11-12 17:02:33 +0000

    Adjust run game function to incorporate inital gamestate object and add this parameter to display question function

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-
   // First page should be visible whilst the other should not
 
   const openingPage = document.getElementById("first-page");
@@ -41,6 +40,7 @@ document.addEventListener("DOMContentLoaded", function () {
         this.getAttribute("data-type") === "false-btn"
       ) {
         //   function that checks the answer
+        checkAnswer(this);
         // function that displays feedback
       } else if (this.getAttribute("data-type") === "next-question-btn") {
         // function which gets next question
@@ -54,19 +54,17 @@ document.addEventListener("DOMContentLoaded", function () {
 // Need a function to increment score
 // Need a function to countdown questions left
 
-
 /**
  * This Function should remove the divs containing the first page and end page, only displaying the ques
  */
-function runGame (object) {
-
+function runGame(object) {
   // Hide the start game and play again page
   // Make the question area page visible
 
   const openingPage = document.getElementById("first-page");
   const quizPage = document.getElementById("game-area");
   const endPage = document.getElementById("quiz-end-page");
-  
+
   if (openingPage.style.display === "block") {
     openingPage.style.removeProperty("display");
     openingPage.style.display = "none";
@@ -75,35 +73,54 @@ function runGame (object) {
   if (quizPage.style.display === "none") {
     quizPage.style.removeProperty("display");
   }
-  
+
   if (endPage.style.display === "block") {
     endPage.style.removeProperty("display");
     endPage.style.display = "none";
   }
 
-  displayQuestion(object);
+  // Store starting point of game variables in an object.
+  const gameState = {
+    questionIndex: 0,
+    questionNumber: 1,
+    questionSet: object.results,
+  };
 
+  // let questionIndex = 0;
+  // const questionSet = object.results;
+
+  displayQuestion(gameState);
 }
 
-function displayQuestion (object) {
+/**
+ * This function should hide the next question button and display the question.
+ */
+function displayQuestion(gameState) {
+  console.log("index:", gameState.questionIndex);
+  console.log("Number:", gameState.questionNumber);
+  console.log("Api object:",gameState.questionSet);
 
-    // Display Question
-    const displayQuestion = document.getElementById("question");
-    displayQuestion.innerText = object.results[0].question;
-    // console.log(object.results[0].question);
+  // Hide the next question button
+  const nextButton = document.getElementById("btn_nextquestion");
+  nextButton.style.display = "none";
 
-    
+  // display the question
+  const questionELement = document.getElementById("question");
+
+  const currentQuestion = gameState.questionSet[gameState.questionIndex.question];
+  // console.log("question:", gameState.questionSet[gameState.questionIndex].question);
+
+  //   if (questionIndex < questionSet.length - 1) {
+  //     const displayQuestion = document.getElementById("question");
+  //     displayQuestion.innerText = object.results[questionIndex].question;
+  //   }
+
+  //   nextQuestion();
 }
 
 /**
  * This function should check which answer was clicked and indicate if the answer was correct
  */
-function checkAnswer() {
+function checkAnswer(button) {}
 
-  
-
-}
-
-function nextQuestion() {
-
-}
+function nextQuestion() {}
